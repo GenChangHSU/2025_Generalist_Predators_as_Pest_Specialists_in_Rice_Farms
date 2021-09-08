820bd9bad8095b1bf7c1b64e697bf1a8547ef4a0
Gen Chang Hsu <ymstrknp@gmail.com>
2021-09-08 06:48:23 -0700

    update

---
+++ R Code/05_Weather.R
@@ -0,0 +1,42 @@
+## -----------------------------------------------------------------------------
+## Title: Daily mean temperature and precipitation of the study site
+##
+## Author: Gen-Chang Hsu
+##
+## Date: 2021-09-08
+##
+## Description: 
+## 1. Plot the daily mean temperature and precipitation of the study site during 
+##    the first rice growth season in year 2017, 2018, and 2019
+##
+## Notes:
+##
+## -----------------------------------------------------------------------------
+set.seed(123)
+
+
+# Libraries --------------------------------------------------------------------
+library(tidyverse)
+library(magrittr)
+
+
+# Import files -----------------------------------------------------------------
+
+
+
+############################### Code starts here ###############################
+
+### Organize the data
+
+
+
+### Plot the data
+
+
+
+
+
+
+
+
+
