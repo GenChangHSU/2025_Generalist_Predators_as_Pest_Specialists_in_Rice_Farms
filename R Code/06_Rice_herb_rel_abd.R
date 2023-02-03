d1ce0746642f809136399d75b7a2ddb57c94ce77
Gen-Chang Hsu <genchanghsu@gmail.com>
2023-02-03 22:38:23 +0800

    u

--- R Code/06_Rice_herb_rel_abd.R
+++ R Code/06_Rice_herb_rel_abd.R
@@ -22,7 +22,7 @@ library(janitor)
 
 
 # Import files -----------------------------------------------------------------
-Abd_2017 <- read.csv("Data_raw/arthropod_abd_2017.csv")
+Abd_2017 <- read.xlsx("Data_raw/arthropod_abd_2017.xlsx", sheetIndex = 1)
 Abd_2018 <- read_xlsx("Data_raw/arthropod_abd_2018_2019.xlsx", sheet = 1)
 Abd_2019 <- read_xlsx("Data_raw/arthropod_abd_2018_2019.xlsx", sheet = 2)
 
@@ -31,13 +31,16 @@ Abd_2019 <- read_xlsx("Data_raw/arthropod_abd_2018_2019.xlsx", sheet = 2)
 
 ### Rice herbivore families in the study years
 Rice_family_2017 <- c("Cic", "Del", "Lyg", "Pen", "Hes", "Pyl", "Nym")
-Abd_2017Rice_family_2018 <- c("Cic", "Del", "Lyg", "Pen", "Hes", "Pyl", "Pyr", "Aly")
+Rice_family_2018 <- c("Cic", "Del", "Lyg", "Pen", "Hes", "Pyl", "Pyr", "Aly")
 Rice_family_2019 <- c("Cic", "Del", "Lyg", "Pen", "Hes", "Pyl", "Pyr", "Aly",
                       "Agr", "Cor", "Mir", "Ric")
 
 Rice_herb_data_2017 <- Abd_2017 %>% 
+  mutate(Farm_type = str_sub(Farm, start = 2, end = 2)) %>%
+  mutate(Farm_type = plyr::mapvalues(Farm_type, from = c("O", "C"), to = c("Organic", "Conventional"))) %>%
+  mutate(Farm_type = factor(Farm_type, levels = c("Organic", "Conventional"), ordered = T)) %>% 
   filter(Family.ID %in% Rice_family_2017) %>%
-  select(Year, Stage, Family = Family.ID, Count) %>%
+  select(Year, Farm_type, Stage, Family = Family.ID, Count) %>%
   mutate(Family = case_when(!(Family %in% c("Cic", "Del", "Lyg", "Pen")) ~ "Others",
                                TRUE ~ Family)) %>%
   filter(Stage %in% c("Flowering", "Ripening")) %>%
@@ -45,8 +48,11 @@ Rice_herb_data_2017 <- Abd_2017 %>%
   
 
 Rice_herb_data_2018 <- Abd_2018 %>% 
+  mutate(Farm_type = str_sub(Farm, start = 2, end = 2)) %>%
+  mutate(Farm_type = plyr::mapvalues(Farm_type, from = c("O", "C"), to = c("Organic", "Conventional"))) %>%
+  mutate(Farm_type = factor(Farm_type, levels = c("Organic", "Conventional"), ordered = T)) %>% 
   filter(Family.abbr %in% Rice_family_2018) %>%
-  select(Date, Family.abbr, Abundance) %>%
+  select(Date, Farm_type, Family.abbr, Abundance) %>%
   mutate(Family.abbr = case_when(!(Family.abbr %in% c("Cic", "Del", "Lyg", "Pen")) ~ "Others",
                                TRUE ~ Family.abbr)) %>%
   mutate(Year = str_sub(Date, start = 1, end = 4),
@@ -54,13 +60,16 @@ Rice_herb_data_2018 <- Abd_2018 %>%
   filter(Date %in% c("0608", "0629")) %>%
   mutate(Stage = case_when(Date == "0608" ~ "Flowering",
                            Date == "0629" ~ "Ripening")) %>%
-  select(Year, Stage, Family = Family.abbr, Count = Abundance)
+  select(Year, Farm_type, Stage, Family = Family.abbr, Count = Abundance)
 
 
 Rice_herb_data_2019 <- Abd_2019 %>% 
+  mutate(Farm_type = str_sub(Farm, start = 2, end = 2)) %>%
+  mutate(Farm_type = plyr::mapvalues(Farm_type, from = c("O", "C"), to = c("Organic", "Conventional"))) %>%
+  mutate(Farm_type = factor(Farm_type, levels = c("Organic", "Conventional"), ordered = T)) %>% 
   mutate(Family.abbr = str_to_title(Family.abbr)) %>%
   filter(Family.abbr %in% Rice_family_2019) %>%
-  select(Date, Family.abbr, Abundance) %>%
+  select(Date, Farm_type, Family.abbr, Abundance) %>%
   mutate(Family.abbr = case_when(!(Family.abbr %in% c("Cic", "Del", "Lyg", "Pen")) ~ "Others",
                                  TRUE ~ Family.abbr)) %>%
   mutate(Year = str_sub(Date, start = 1, end = 4),
@@ -68,23 +77,20 @@ Rice_herb_data_2019 <- Abd_2019 %>%
   filter(Date %in% c("0620", "0702")) %>%
   mutate(Stage = case_when(Date == "0620" ~ "Flowering",
                            Date == "0702" ~ "Ripening")) %>%
-  select(Year, Stage, Family = Family.abbr, Count = Abundance)
+  select(Year, Farm_type, Stage, Family = Family.abbr, Count = Abundance, )
 
 
 bind_rows(Rice_herb_data_2017, 
           Rice_herb_data_2018,
           Rice_herb_data_2019) %>%
-  group_by(Year, Stage, Family) %>%
+  group_by(Year, Farm_type, Stage, Family) %>%
   summarise(N = sum(Count)) %>%
   mutate(Prop = round(N/sum(N), 3)*100,
          Prop = paste0(Prop, "%"),
          Family = factor(Family, levels = c("Cic", "Del", "Lyg", "Pen", "Others"))) %>%
   select(-N) %>% 
-  full_join(., expand(., Year, Stage, Family)) %>%
-  arrange(Year, Stage, Family) %>%
-  pivot_wider(names_from = Year, values_from = Prop) %>%
+  pivot_wider(names_from = c(Year, Farm_type), values_from = Prop) %>%
+  arrange(Stage, Family) %>%
   write_csv("./Output/Data_clean/Rice_herb_rel_abd.csv")
   
-
-
 
